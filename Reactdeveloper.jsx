f09534b8e8d35491c71df7f8de405104f6bec134
Poojitha-Bonam <poojithabonam@gmail.com>
2025-10-07 22:59:19 +0530

    Reactdeveloper.jsx

--- Reactdeveloper.jsx
+++ Reactdeveloper.jsx
@@ -1,6 +1,7 @@
 // Function name should starts with capital letter
 function Reactdeveloper(){
-    return <h1>I am React Developer</h1>
+    return <h1>Hello I am React Developer</h1>
 }
+
 
 export default Reactdeveloper
